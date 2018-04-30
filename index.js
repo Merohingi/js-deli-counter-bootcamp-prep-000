8f8872f1ba2c9f44f20f2a745c31d5d7d0ba7450
Marvin Bonds <mbonds1@mail.depaul.edu>
2018-04-30 09:10:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,16 @@ function nowServing(katzDeliLine) {
   }
   }
   
-  
+  function currentLine(line) {
+  if (line.length != 0) {
+    return "The line is currently empty.";
+  }
+
+  else {.
+    var newArray = [];
+    for (var i = 0; i < line.length; i++){
+      newArray.push(` ${i + 1}. ${line[i]}`);
+    }
+  }
+return `The line is currently:${newArray}`;
+}
